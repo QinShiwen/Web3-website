3427d9f20ddedf5e126ea789b259f4def1309936
QinShiwen <719087992@qq.com>
2022-11-29 00:43:06 +0800

    main_page

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -1,14 +1,326 @@
-
 import styled from "styled-components";
+import { Space } from "antd";
+import detopiaIcon from "../../assets/Detopia.png";
+import Avatar from "../../assets/Avatar.png";
+import searchIcon from "../../assets/search.png";
+import projectData from "./data";
 
-export const GameHome = () =>{
-    return (
-        <Container>
+export const GameHome = () => {
+  const { head, gameNews, bestGame, bottomInfo } = projectData;
 
-        </Container>
-    )
-}
+  function locateLink(link: string) {
+    window.location.href = link;
+  }
+  return (
+    <Container>
+      <Header>
+        <div className="icon">
+          <img src={detopiaIcon} width={67} height={70}></img>
+        </div>
+        <Space size={80}>
+          <div>Home</div>
+          <div>About us</div>
+          <div>For You</div>
+          <div>Arcade</div>
+        </Space>
+        <div className="avatar">
+          <img src={Avatar} width={104} height={104} />
+        </div>
+      </Header>
+      <Body>
+        <div className="searching-box">
+          <h2>{head.title}</h2>
+          <h3>{head.content}</h3>
+          <div className="input-box">
+            <img src={searchIcon} width={15} />
+            <input placeholder="search" />
+          </div>
+        </div>
+        <InfoBox>
+          <div className="info-left">
+            {gameNews.map((item, index) => {
+              return (
+                <div className="game-news">
+                  <img src={item.image} width={587} height={295} />
+                  <div>
+                    <span className="news-title">
+                      <img src={item.avatar} width={77} height={77} />
+                      <button>Join Game</button>
+                      <h4>{item.tag}</h4>
+                    </span>
+                    <div>
+                      <h2>{item.title}</h2>
+                      <h5>{item.content}</h5>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="info-right">
+            <h2>Best Game</h2>
+            <Space direction="vertical">
+              {bestGame.map((item, index) => {
+                return (
+                  <div className="best-game">
+                    <div>
+                      <img
+                        src={item.image}
+                        width={140}
+                        onClick={() => locateLink(item.link)}
+                      />
+                    </div>
+                    <div>
+                      <div className="tag-box">
+                        <button onClick={() => locateLink(item.link)}>
+                          Join Game
+                        </button>
+                        <h4>{item.tag}</h4>
+                      </div>
+                      <h3>{item.titile}</h3>
+                    </div>
+                  </div>
+                );
+              })}
+            </Space>
+          </div>
+        </InfoBox>
+      </Body>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    
-`
+  width: 100vw;
+  background: linear-gradient(176.03deg, #c10580 -34.57%, #030115 96.75%);
+  display: flex;
+  flex-direction: column;
+  aligin-items: center;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  margin: 50px 0;
+  .ant-space {
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex: 0.7;
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 23px;
+    line-height: 34px;
+
+    color: #ffffff;
+  }
+  .icon {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex: 0.15;
+  }
+  .avatar {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex: 0.15;
+  }
+`;
+
+const Body = styled.div`
+  width: 100vw;
+  .searching-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h2 {
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 44px;
+    text-align: center;
+    color: #ffffff;
+    margin: 30px 0 20px 0;
+  }
+
+  h3 {
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    text-align: center;
+    margin: 10px 0 80px 0;
+    color: #ffffff;
+  }
+
+  .input-box {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background: #210033;
+    border-radius: 10px;
+    padding-left: 20px;
+    input {
+      width: 380px;
+      height: 49px;
+      outline: none;
+      border: none;
+      background: #210033;
+      border-radius: 10px;
+      margin-left: 10px;
+
+      font-family: "Poppins";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      color: #ffffff;
+    }
+  }
+`;
+
+const InfoBox = styled.div`
+  margin: 100px;
+  display: flex;
+  flex-direction: row;
+
+  .info-left {
+    flex: 0.45;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 20px;
+
+    .game-news {
+      padding: 30px;
+    }
+
+    .news-title {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      margin: 30px 0 10px 0;
+
+      button {
+        width: 164.04px;
+        height: 48px;
+        border: none;
+        background: #fa9021;
+        margin: 0 20px;
+        font-family: "Poppins";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 187.5%;
+        /* or 26px */
+
+        color: #ffffff;
+      }
+
+      h4 {
+        font-family: "Poppins";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 187.5%;
+        /* identical to box height, or 30px */
+
+        color: #ffffff;
+      }
+    }
+
+    h2 {
+      font-family: "Poppins";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 30px;
+      line-height: 45px;
+      /* identical to box height */
+      text-align: initial;
+      color: #ffffff;
+    }
+
+    h5 {
+      text-align: initial;
+      color: #ffffff;
+      font-family: "Poppins";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 17px;
+      line-height: 207.8%;
+      padding: 0 30px 0 0;
+    }
+  }
+
+  .info-right {
+    flex: 0.55;
+    background: rgba(33, 0, 51, 0.4);
+    border-radius: 30px;
+    padding: 50px;
+    h2 {
+    }
+
+    .best-game {
+      display: flex;
+      flex-direction: row;
+      margin: 30px 50px 0 20px;
+      button {
+        width: 137.97px;
+        height: 39.9px;
+        background: #d32f67;
+        border: none;
+        font-family: "Poppins";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 187.5%;
+
+        color: #ffffff;
+      }
+
+      h3 {
+        font-family: "Poppins";
+        font-style: normal;
+        font-weight: 600;
+        font-size: 23px;
+        line-height: 34px;
+        text-align: initial;
+        color: #ffffff;
+      }
+
+      h4 {
+        font-family: "Poppins";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 187.5%;
+        color: #ffffff;
+      }
+
+      img {
+        margin: 0 80px 0 30px;
+        cursor: pointer;
+      }
+
+      .tag-box {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        button {
+          margin-right: 50px;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`;
--- src/pages/main/data.ts
+++ src/pages/main/data.ts
@@ -1,46 +1,77 @@
+import gameCyball from "../../assets/game-Cyball.png";
+import gameHarvest from "../../assets/game-harvest.png";
+import gameStar from "../../assets/game-star.png";
+import gameIllu from "../../assets/game-Illuv.png";
+import gameMomo from "../../assets/game-MMO.png";
 
-import gameCyball from "../../assets/game-Cyball.png"
-import gameHarvest from "../../assets/game-harvest.png"
-import gameStar from "../../assets/game-star.png"
-import gameIllu from "../../assets/game-Illuv.png"
-import gameMomo from "../../assets/game-Illuv.png"
+import gameIlluNew from "../../assets/game-illu-intro.png";
+import gameIlluAvatar from "../../assets/game-illu-avatar.png";
+import gameStarIntro from "../../assets/game-star-intro.png";
 
-export const projectData = {
-    bestGame:[
-        {
-            image:gameHarvest, 
-            titile:"The Harvest is a fresh new take on Class-Based Shooters.",
-            link:"https://theharvestgame.com/",
-            tag:"30,000 players"
-        },      
-        {
-            image:gameStar,
-            titile:"Star Atlas A grand strategy game of space exploration, territrial conquest, political domination, and more.",
-            link:"https://staratlas.com/",
-            tag:"PRE-ALPHA"
-        },      
-        {
-            image:gameIllu,
-            titile:"Illuvium, the world's first IBG (Interoperable Blockchain Game) is an upcoming open-world exploration.",
-            link:"https://illuvium.io/",
-            tag:"80,000 players"
-        },         
-        {
-            image:gameMomo,
-            titile:"The first MMO game of earning cryptocurrency and blowing up cars.",
-            link:"https://warriders.com/",
-            tag:"70,000 players"
-        },        
-        {
-            image:gameCyball,
-            titile:"CyBall is a football-themed, NFT-based blockchain game with a Play-to-Earn model.",
-            link:"https://www.medievalempires.com/",
-            tag:"70,00 players"
-        },                                                                                         
-    ],
-    bottomInfo:{
-        aboutUs:[
+import gameStarAvatar from "../../assets/game-star-avatar.png";
 
-        ]
+
+
+const projectData = {
+  head: {
+    title: "Enjoy your Play and Earn in Web3",
+    content: "Let's pick up the best Web3 Game together.",
+  },
+  gameNews:[
+    {
+        image:gameStarIntro,
+        avatar:gameStarAvatar,
+        tag:"5 million",
+        title:"Star Atlas White Paper",
+        content:"Star Atlas is a virtual gaming metaverse based in the year 2620.In this distant future, three major factions have emerged; The MUD Territory governed by humankind, The ONI Region as a consortium of alien races, and The Ustur Sector controlled by sentient androids. These factions are in an ongoing struggle for resources, territorial conquest, and political domination. You, a faction citizen of Star Atlas, will have the ability to influence the outcome of this intergalactic conflict while creating the opportunity to earn rewards for your contributions."
+    },
+    {
+        image:gameIlluNew,
+        avatar:gameIlluAvatar,
+        tag:"5 million",
+        title:"Illuvium White Paper",
+        content:"Illuvium, the world's first IBG(Interoperable Blockchain Game) is an upcomingopen-world exploration,NFT creature collector andautobattler game built on the Ethereum blockchain.Join a graphically-rich sci-fi adventure, explore 7 dazzling alien landscapes and conquer the wilderness to uncover the cataclysmic events that shattered Illuvium and help your crash-landed crew flourish!"
     }
-}
+  ],
+  bestGame: [
+    {
+      image: gameHarvest,
+      titile: "The Harvest is a fresh new take on Class-Based Shooters.",
+      link: "https://theharvestgame.com/",
+      tag: "30,000 players",
+    },
+    {
+      image: gameStar,
+      titile:
+        "Star Atlas A grand strategy game of space exploration, territrial conquest, political domination, and more.",
+      link: "https://staratlas.com/",
+      tag: "PRE-ALPHA",
+    },
+    {
+      image: gameIllu,
+      titile:
+        "Illuvium, the world's first IBG (Interoperable Blockchain Game) is an upcoming open-world exploration.",
+      link: "https://illuvium.io/",
+      tag: "80,000 players",
+    },
+    {
+      image: gameMomo,
+      titile:
+        "The first MMO game of earning cryptocurrency and blowing up cars.",
+      link: "https://warriders.com/",
+      tag: "70,000 players",
+    },
+    {
+      image: gameCyball,
+      titile:
+        "CyBall is a football-themed, NFT-based blockchain game with a Play-to-Earn model.",
+      link: "https://www.medievalempires.com/",
+      tag: "70,00 players",
+    },
+  ],
+  bottomInfo: {
+    aboutUs: [],
+  },
+};
+
+export default projectData;
